ff0b25e56ec78980cc587b73cee2fed3cac5dab5
Sean Reifschneider <jafo@tummy.com>
2011-09-09 18:39:37 -0600

    Fixing login looping bug, adding logging.
    On the first request, if an auth token has not been received, a login will
    be done.
    
    If the login fails, it raises a LoginFailed exception.
    
    Added a debug() function to enable some logging.  Currently only writes to
    stdout.

--- dynect_client.py
+++ dynect_client.py
@@ -1,4 +1,19 @@
-import simplejson, urllib2, urllib
+import simplejson, urllib2, urllib, sys
+
+class LoginFailed(Exception):
+  '''Exception raised when a login fails.  Includes these attributes:
+
+    - status : The status returned by the server.
+    - msgs : A list of dictionaries with information about the failure,
+        which include the keys "INFO", "SOURCE", "ERR_CD", and "LVL" (level).
+
+    If the login failed with an HTTP error, `msgs` will an empty list, and
+    `status` will be 'HTTP Error'.
+    '''
+  def __init__(self, status, msgs):
+    self.status = status
+    self.msgs = msgs
+
 
 class DynectDNSClient:
   def __init__(self, customerName, userName, password, defaultDomain=None):
@@ -7,6 +22,22 @@ class DynectDNSClient:
     self.password = password
     self.defaultDomainName = defaultDomain
     self.sessionToken = None
+    self.debugEnabled = False
+
+
+  def debug(self, enable = True):
+    '''Enable or disable debugging messages.  If called with no arguments or
+    with the argument `True`, debugging is turned on, if `False` is passed
+    debugging is disabled.'''
+    self.debugEnabled = enable
+
+
+  def _log(self, msg):
+    'INTERNAL: Log a message if the debugging flag is enabled.'
+    if not self.debugEnabled:
+      return
+    sys.stderr.write('LOG: ' + msg.rstrip() + '\n')
+
 
   def getRecords(self, hostName, type="A", domainName=None):
     if not domainName:
@@ -63,20 +94,33 @@ class DynectDNSClient:
       return ("CNameRecord", "cname")
 
 
-
   def _publish(self, domainName=None):
     self._request("Zone/%s" % domainName, {"publish": True}, type="PUT")
 
+
   def _login(self):
-    response = self._request("Session/", {'customer_name': self.customerName,
-                                                'user_name': self.userName,
-                                                'password': self.password})
+    try:
+      response = self._request("Session/", {'customer_name': self.customerName,
+                                                  'user_name': self.userName,
+                                                  'password': self.password})
+    except urllib2.HTTPError, e:
+      if e.code == 400:
+        self._log('Login failed with HTTP 400 error, likely an account issue')
+        raise LoginFailed(status = 'HTTP Error', msgs = [])
+      raise
+
     if response['status'] != 'success':
-      return
+      self._log('Login failed due to "status" of "%s"' % response['status'])
+      raise LoginFailed(status = response['status'], msgs = response['msgs'])
     self.sessionToken = response['data']['token']
 
   def _request(self, url, post, type=None):
+    self._log('_request(url="%s", post="%s", type="%s")' % ( url, post, type ))
     fullurl = "https://api2.dynect.net/REST/%s" % url
+
+    if not self.sessionToken and url != 'Session/':
+      self._log('Doing login...')
+      self._login()
 
     if post:
       postdata = simplejson.dumps(post)
@@ -89,19 +133,14 @@ class DynectDNSClient:
     if type:
       setattr(req, "method", type)
 
-    try:
-      resp = urllib2.urlopen(req)
-      if type:
-        return resp
-      else:
-        return simplejson.loads(resp.read())
+    resp = urllib2.urlopen(req)
+    if type:
+      return resp
+    else:
+      data = resp.read()
+      self._log('JSON Response: "%s"' % data)
+      return simplejson.loads(data)
 
-    except urllib2.HTTPError, e:
-      if e.code == 400:
-        self._login()
-        return self._request(url, post)
-      else:
-        raise e
 
 class MethodRequest(urllib2.Request):
   def __init__(self, *args, **kwargs):
@@ -112,5 +151,3 @@ class MethodRequest(urllib2.Request):
     if self.method:
       return self.method
     return urllib2.Request.get_method(self)
-
-
